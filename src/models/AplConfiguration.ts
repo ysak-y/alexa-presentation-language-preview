d03d1ac79a7e5e940e32e4df09ca8408a182f964
ysak-y <yoshiaki.0614@gmail.com>
2022-08-28 15:54:48 +0900

    Add setAndInflateAplPayload method to AplConfiguration. This method is for importing package from local direcotry

--- src/models/AplConfiguration.ts
+++ src/models/AplConfiguration.ts
@@ -1,4 +1,8 @@
+import { LocalPackageImportError } from "./../utils/exceptions";
 import { IViewport, getDefaultViewport } from "apl-suggester";
+import * as fs from "node:fs";
+import merge from "merge";
+import * as path from "node:path";
 
 type JsonValue =
   | string
@@ -7,9 +11,24 @@ type JsonValue =
   | JsonValue[]
   | { [key: string]: JsonValue };
 
-interface AplPayload {
+export interface AplPayload {
   document: { [key: string]: JsonValue };
   datasources: { [key: string]: JsonValue };
+}
+
+interface AplPackageImport {
+  name: string;
+  version: string;
+  source: string;
+}
+
+function isAplPackageArray(
+  aplPackageArray: any
+): aplPackageArray is AplPackageImport[] {
+  return (
+    Array.isArray(aplPackageArray) &&
+    aplPackageArray.every((a) => a.name && a.version)
+  );
 }
 
 export class AplConfiguration {
@@ -21,5 +40,67 @@ export class AplConfiguration {
     this.aplPayload = aplPayload
       ? aplPayload
       : { document: {}, datasources: {} };
+  }
+
+  setAndInflateAplPayload(
+    documentDirecotyrPath: string,
+    aplPayload: AplPayload
+  ) {
+    const pacakgeJson = aplPayload.document["import"];
+    const newPackageJson: JsonValue[] = [];
+    if (pacakgeJson && isAplPackageArray(pacakgeJson)) {
+      pacakgeJson.forEach((p: AplPackageImport) => {
+        if (!p.source) {
+          newPackageJson.push(JSON.parse(JSON.stringify(p)));
+          return;
+        }
+
+        const document = this.loadLocalPackage(
+          path.resolve(documentDirecotyrPath, p.source)
+        );
+
+        if (!document) {
+          newPackageJson.push(JSON.parse(JSON.stringify(p)));
+          return;
+        }
+
+        // Merge each porperty of the package
+        Object.keys(document).forEach((k) => {
+          // Shouldn't merge mainTemplate with packages
+          if (k === "mainTemplate") {
+            return;
+          }
+
+          aplPayload.document[k] = merge(
+            true,
+            aplPayload.document[k],
+            document[k]
+          );
+        });
+      });
+
+      // newPackageJson doesn't include local pacakge imports because would happen error while rendering APL document
+      aplPayload.document["import"] = newPackageJson;
+
+      this.aplPayload = aplPayload;
+    }
+  }
+
+  private loadLocalPackage(packagePath: string) {
+    try {
+      if (!fs.existsSync(packagePath)) {
+        return;
+      }
+
+      const file = fs.readFileSync(packagePath);
+      return JSON.parse(file.toString());
+    } catch (e) {
+      let errorMessage = "Error while importing local pacakge.";
+      if (e instanceof Error) {
+        errorMessage += ` Reason: ${e.message}`;
+      }
+
+      throw new LocalPackageImportError(errorMessage);
+    }
   }
 }
