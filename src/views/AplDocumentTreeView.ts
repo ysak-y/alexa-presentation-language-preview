4bc092d199b2572536695073a2ccac9a3b0bd5a7
ysak-y <yoshiaki.0614@gmail.com>
2022-09-04 13:51:44 +0900

    Show only component layer in apl document tree

--- src/views/AplDocumentTreeView.ts
+++ src/views/AplDocumentTreeView.ts
@@ -1,158 +1,89 @@
-import { isJsonType, JsonValue } from "./../utils/JsonUtils";
 import * as vscode from "vscode";
 import { JsonType } from "../utils/JsonUtils";
 import { AplConfiguration } from "../models/AplConfiguration";
-import * as AplSuggestor from "apl-suggester";
 
 export class AplDocumentTreeView
-  implements vscode.TreeDataProvider<AplPropertyItem | AplComponentItem>
+  implements vscode.TreeDataProvider<AplComponent>
 {
   constructor(private aplConfiguration: AplConfiguration) {}
 
-  getTreeItem(element: AplPropertyItem | AplComponentItem): vscode.TreeItem {
+  getTreeItem(element: AplComponent): vscode.TreeItem {
     return element;
   }
 
-  getChildren(
-    element?: AplPropertyItem | AplComponentItem
-  ): Thenable<AplPropertyItem[] | AplComponentItem[]> {
-    if (!element) {
-      return this.buildRootItem();
-    }
-
-    if (element instanceof AplPropertyItem) {
-      // element が text や width などのプロパティの時
-      if (
-        ["items", "item"].includes(element.label) &&
-        element.value &&
-        Array.isArray(element.value)
-      ) {
-        const filteredArray: JsonType[] = element.value.filter((v) =>
-          isJsonType(v)
-        ) as JsonType[];
-
-        const childComponents: AplComponentItem[] = filteredArray.map(
-          (c: JsonType) => {
-            const label = c["type"] as string;
-            delete c["type"];
-            return new AplComponentItem(
-              label,
-              vscode.TreeItemCollapsibleState.None,
-              c
-            );
-          }
-        );
-        return Promise.resolve(childComponents);
+  getChildren(element?: AplComponent): Thenable<AplComponent[]> {
+    if (element) {
+      const childComponents: AplComponent[] = this.getItemsFromComponents(
+        element.properties
+      ).map((c: JsonType) => {
+        const label = (c["id"] ? c["id"] : c["type"]) as string;
+        const collapsibleState =
+          Object.keys(c).length === 0
+            ? vscode.TreeItemCollapsibleState.None
+            : vscode.TreeItemCollapsibleState.Expanded;
+        return new AplComponent(label, c, collapsibleState);
+      });
+      return Promise.resolve(childComponents);
+    } else {
+      const mainTemplate = this.aplConfiguration.aplPayload.document[
+        "mainTemplate"
+      ] as JsonType;
+      if (mainTemplate) {
+        const collapsibleState =
+          mainTemplate.length === 0
+            ? vscode.TreeItemCollapsibleState.None
+            : vscode.TreeItemCollapsibleState.Expanded;
+        return Promise.resolve([
+          new AplComponent("mainTemplate", mainTemplate, collapsibleState),
+        ]);
       } else {
         return Promise.resolve([]);
       }
-    } else if (element instanceof AplComponentItem) {
-      // element が Text や Container などのコンポーネントの時
-      return AplSuggestor.ComponentSchemaController.getInstance()
-        .getComponentSchema(this.aplConfiguration.aplPayload, element.label)
-        .then((componentProperties) => {
-          const elementPropertyKeys = element.properties
-            ? Object.keys(element.properties)
-            : [];
-
-          const mergedPropertyKeys = elementPropertyKeys.concat(
-            Object.keys(
-              componentProperties?.properties
-                ? componentProperties.properties
-                : {}
-            )
-          );
-
-          const childComponents = mergedPropertyKeys.map((c) => {
-            return new AplPropertyItem(
-              c,
-              vscode.TreeItemCollapsibleState.None,
-              element.properties[c]
-            );
-          });
-          return Promise.resolve(childComponents);
-        });
-    } else {
-      return this.buildRootItem();
     }
-  }
-
-  private async buildRootItem(): Promise<AplComponentItem[]> {
-    const mainTemplate =
-      this.aplConfiguration.aplPayload.document["mainTemplate"];
-
-    if (!mainTemplate) {
-      return Promise.resolve([]);
-    }
-
-    const collapsibleState =
-      Object.keys(mainTemplate).length === 0
-        ? vscode.TreeItemCollapsibleState.None
-        : vscode.TreeItemCollapsibleState.Expanded;
-
-    return Promise.resolve([
-      new AplComponentItem(
-        "mainTemplate",
-        collapsibleState,
-        mainTemplate as JsonType
-      ),
-    ]);
   }
 
   private _onDidChangeTreeData: vscode.EventEmitter<
-    AplComponentItem | AplPropertyItem | undefined | null | void
-  > = new vscode.EventEmitter<
-    AplComponentItem | AplPropertyItem | undefined | null | void
-  >();
+    AplComponent | undefined | null | void
+  > = new vscode.EventEmitter<AplComponent | undefined | null | void>();
   readonly onDidChangeTreeData: vscode.Event<
-    AplComponentItem | AplPropertyItem | undefined | null | void
+    AplComponent | undefined | null | void
   > = this._onDidChangeTreeData.event;
 
   refresh(): void {
     this._onDidChangeTreeData.fire();
   }
 
-  //private getItemsFromComponents(aplComponent: JsonType) {
-  //  let items: JsonType[] = [];
-  //  const itemsValue = this.wrapWithArrayIfObject(aplComponent["items"]);
-  //  if (itemsValue) {
-  //    items = items.concat(itemsValue);
-  //  }
+  private getItemsFromComponents(aplComponent: JsonType) {
+    let items: JsonType[] = [];
+    const itemsValue = this.wrapWithArrayIfObject(aplComponent["items"]);
+    if (itemsValue) {
+      items = items.concat(itemsValue);
+    }
 
-  //  const itemValue = this.wrapWithArrayIfObject(aplComponent["item"]);
-  //  if (itemValue) {
-  //    items = items.concat(itemValue);
-  //  }
+    const itemValue = this.wrapWithArrayIfObject(aplComponent["item"]);
+    if (itemValue) {
+      items = items.concat(itemValue);
+    }
 
-  //  return items;
-  //}
+    return items;
+  }
 
-  //private wrapWithArrayIfObject(
-  //  val: JsonType[] | Object
-  //): JsonType[] | undefined {
-  //  if (Array.isArray(val)) {
-  //    return val;
-  //  } else if (typeof val === "object") {
-  //    return [val as JsonType];
-  //  }
-  //}
-}
-
-class AplPropertyItem extends vscode.TreeItem {
-  constructor(
-    public readonly label: string,
-    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-    public readonly value: JsonValue | null
-  ) {
-    super(label, collapsibleState);
+  private wrapWithArrayIfObject(
+    val: JsonType[] | Object
+  ): JsonType[] | undefined {
+    if (Array.isArray(val)) {
+      return val;
+    } else if (typeof val === "object") {
+      return [val as JsonType];
+    }
   }
 }
 
-class AplComponentItem extends vscode.TreeItem {
+class AplComponent extends vscode.TreeItem {
   constructor(
     public readonly label: string,
-    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-    public readonly properties: JsonType
+    public properties: JsonType,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState
   ) {
     super(label, collapsibleState);
   }
