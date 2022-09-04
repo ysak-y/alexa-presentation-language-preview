dbab1d99a07a7b188d25033544b539359816fe70
ysak-y <yoshiaki.0614@gmail.com>
2022-09-04 13:30:09 +0900

    Implement util functions for json type checking in JsonUtil

--- src/utils/JsonUtils.ts
+++ src/utils/JsonUtils.ts
@@ -1,8 +1,23 @@
-export type JsonValue =
-  | string
-  | number
-  | boolean
-  | JsonValue[]
-  | { [key: string]: JsonValue };
-
 export type JsonType = { [key: string]: JsonValue };
+
+export type JsonValue = string | number | boolean | JsonValue[] | JsonType;
+
+export function isJsonValue(val: unknown): val is JsonValue {
+  return (
+    typeof val === "string" ||
+    typeof val === "number" ||
+    typeof val === "boolean" ||
+    (Array.isArray(val) && val.every((v) => isJsonValue(v))) ||
+    (!!val &&
+      typeof val === "object" &&
+      Object.keys(val).every((v) => isJsonValue(v)))
+  );
+}
+
+export function isJsonType(val: unknown): val is JsonType {
+  return (
+    !!val &&
+    typeof val === "object" &&
+    Object.keys(val).every((v) => isJsonValue(v))
+  );
+}
